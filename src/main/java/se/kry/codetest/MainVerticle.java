3a37dd3eeabb2919c6cae85e0823af30d9ec8b3b
saurabh parmar <saurabh.parmar08@gmail.com>
2020-05-28 09:19:06 +0200

    Refactoring of code

--- src/main/java/se/kry/codetest/MainVerticle.java
+++ src/main/java/se/kry/codetest/MainVerticle.java
@@ -4,7 +4,6 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
-import io.vertx.ext.sql.ResultSet;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -12,23 +11,24 @@ import io.vertx.ext.web.handler.StaticHandler;
 import se.kry.codetest.migrate.DBMigration;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private Map<String, AppService> serviceCache = new HashMap<>();
-  private AppServiceRepository appServiceRepository;
+  private AppServiceHelper appServiceHelper;
   private DBMigration migration;
   private final BackgroundPoller poller = new BackgroundPoller();
 
   @Override
   public void start(Future<Void> startFuture) {
-    appServiceRepository = new AppServiceRepository(new DBConnector(vertx));
+    appServiceHelper = new AppServiceHelper(new DBConnector(vertx));
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
     setRoutes(router);
     configureServer(startFuture, router);
-    loadServiceCache();
+    appServiceHelper.loadServiceCache();
   }
 
   private void configureServer(Future<Void> startFuture, Router router) {
@@ -55,12 +55,12 @@ public class MainVerticle extends AbstractVerticle {
   }
 
   private void enableGetServiceRoute(Router router) {
-    router.get("/service").handler(req -> setSuccessResponse(req, Optional.of(new JsonArray(new ArrayList<>(serviceCache.values())))));
+    router.get("/service").handler(req -> setSuccessResponse(req, Optional.of(new JsonArray(new ArrayList<>(appServiceHelper.getServiceCache().values())))));
   }
 
   private void enableGetPollServiceRoute(Router router) {
     router.get("/poll").handler(req -> {
-      List<AppService> services = new ArrayList<>(serviceCache.values());
+      List<AppService> services = new ArrayList<>(appServiceHelper.getServiceCache().values());
       poller.pollServicesAndUpdateCache(services);
       setSuccessResponse(req, Optional.of(new JsonArray(services)));
     });
@@ -70,8 +70,8 @@ public class MainVerticle extends AbstractVerticle {
     router.delete("/service").handler(req -> {
       String url = req.getBodyAsJson().getString("url");
       JsonArray params = new JsonArray().add(url);
-      deleteAppServiceByUrl(params);
-      serviceCache.remove(url);
+      appServiceHelper.deleteAppServiceByUrl(params);
+      appServiceHelper.getServiceCache().remove(url);
       setSuccessResponse(req, Optional.empty());
     });
   }
@@ -80,7 +80,7 @@ public class MainVerticle extends AbstractVerticle {
     router.post("/service").handler(req -> {
       String name = req.getBodyAsJson().getString("name");
       String url = req.getBodyAsJson().getString("url");
-      if(serviceCache.containsKey(url)){
+      if(appServiceHelper.getServiceCache().containsKey(url)){
         setErrorResponse(req, "URL already exist");
         return;
       }
@@ -88,8 +88,8 @@ public class MainVerticle extends AbstractVerticle {
                             .add(name)
                             .add(url)
                             .add(LocalDateTime.now().toString());
-      saveAppService(params);
-      serviceCache.put(url, new AppService(name, url));
+      appServiceHelper.saveAppService(params);
+      appServiceHelper.getServiceCache().put(url, new AppService(name, url));
       setSuccessResponse(req, Optional.empty());
     });
   }
@@ -101,8 +101,8 @@ public class MainVerticle extends AbstractVerticle {
       JsonArray params = new JsonArray()
               .add(name)
               .add(url);
-      appServiceRepository.updateAppService(params);
-      serviceCache.put(url, new AppService(name, url));
+      appServiceHelper.updateAppService(params);
+      appServiceHelper.getServiceCache().put(url, new AppService(name, url));
       setSuccessResponse(req, Optional.empty());
     });
   }
@@ -121,37 +121,8 @@ public class MainVerticle extends AbstractVerticle {
     req.response().setStatusCode(HttpResponseStatus.CONFLICT.code()).end(message);
   }
 
-  public void deleteAppServiceByUrl(JsonArray params) {
-    appServiceRepository.deleteAppServiceByUrl(params);
-  }
 
 
-  public void saveAppService(JsonArray params) {
-    appServiceRepository.saveAppService(params);
-  }
 
-  private void loadServiceCache() {
-    Future<ResultSet> services = appServiceRepository.getAllServices();
-    Map<String, AppService> result = new HashMap<>();
-    services.setHandler(done -> {
-      if (done.succeeded()) {
-        System.out.println(done.result().getResults() + "********done.result().getResults()");
-        done.result().getResults()
-                .forEach(json -> result.put(json.getString(0), convertResultToAppService(json)));
-      } else {
-        System.out.println(done.cause().getStackTrace());
-      }
-    serviceCache = result;
-    });
-  }
-
-  private AppService convertResultToAppService(JsonArray json) {
-    String url = json.getString(0);
-    String name = json.getString(1);
-    String creationDate = json.getString(2);
-    AppService service =  new AppService(name, url);
-    service.setCreationDate(creationDate);
-    return service;
-  }
 
 }
